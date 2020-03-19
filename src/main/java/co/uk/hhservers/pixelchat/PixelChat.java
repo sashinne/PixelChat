77ec4762da11154da67b84d621285a01aaf6f68f
B <hennyhexservers@gmail.com>
2020-03-19 19:45:38 +0000

    moar cleanup

--- src/main/java/co/uk/hhservers/pixelchat/PixelChat.java
+++ src/main/java/co/uk/hhservers/pixelchat/PixelChat.java
@@ -46,30 +46,7 @@ public class PixelChat {
             Character slot = msg.charAt(msg.length() - 1);
             Integer slotInt = Integer.parseInt(slot.toString());
             Pokemon pokemon = party.get(slotInt);
-            String pokeName = pokemon.getDisplayName();
-            //Integer pokeLevel = pokemon.getLevel();
-            //EVStore evStore = pokemon.getEVs();
-            //IVStore ivStore = pokemon.getIVs();
-            //String itemTypeID = "pixelmon:pixelmon_sprite";
-            //Optional<ItemType> opItem = Sponge.getRegistry().getType(ItemType.class, itemTypeID);
-            //ItemStackSnapshot itemSnapshot = opItem.get().getTemplate();
-            //ItemStack realSprite = itemSnapshot.createStack();
-            //Text EVTemplate = TextSerializers.FORMATTING_CODE.deserialize("&aAttack:&d " + evStore.attack + "\n&aDefence:&d " + evStore.defence + "\n&aSpeed:&d " + evStore.speed + "\n&aSpAtk:&d " + evStore.specialAttack + "\n&aSpDef:&d " + evStore.specialDefence);
-            //Text evs = Text.builder("&l&8[&r&aEVs&r&l&8]&r").onHover(TextActions.showText(EVTemplate)).build();
-            //Text IVTemplate = TextSerializers.FORMATTING_CODE.deserialize("&aAttack:&d " + ivStore.attack + "\n&aDefence:&d " + ivStore.defence + "\n&aSpeed:&d " + ivStore.speed + "\n&aSpAtk:&d " + ivStore.specialAttack + "\n&aSpDef:&d " + ivStore.specialDefence);
-            //Text ivs = Text.builder("[").color(TextColors.DARK_GRAY).append(Text.builder("IVs").color(TextColors.GREEN).build()).append(Text.builder("]").color(TextColors.DARK_GRAY).build()).onHover(TextActions.showText(IVTemplate)).build();
-            //Text evs = Text.builder("[").color(TextColors.DARK_GRAY).append(Text.builder("EVs").color(TextColors.GREEN).build()).append(Text.builder("]").color(TextColors.DARK_GRAY).build()).onHover(TextActions.showText(EVTemplate)).build();
             event.setCancelled(true);
-            World world = player.getWorld();
-            /*Text messageathon = TextSerializers.FORMATTING_CODE.deserialize("&l&8[&r&a" + pokeName + "&l&8]&r").toBuilder()
-                    .onHover(TextActions.showText(TextSerializers.FORMATTING_CODE.deserialize("&aGender:&d " + pokemon.getGender().toString()
-                            + "\n&aLevel:&d " + pokeLevel.toString()
-                            + "\n&aAbility:&d " + pokemon.getAbilityName()
-                            + "\n&aNature:&d " + pokemon.getNature().name()
-                            + "\n&aHappiness:&d " + pokemon.getFriendship())))
-                    .append(evs)
-                    .append(ivs)
-                    .build();*/
             Text wat = PokeData.getHoverText(pokemon);
             Text finalmessage = Text.builder("[").color(TextColors.DARK_GRAY)
                     .append(Text.builder(player.getName()).color(TextColors.LIGHT_PURPLE).build())
@@ -80,11 +57,9 @@ public class PixelChat {
             MessageChannel.TO_PLAYERS.send(finalmessage);
         }
         if (msg.toLowerCase().startsWith("@party")) {
-
             UUID uuid = player.getUniqueId();
             PartyStorage playerParty = Pixelmon.storageManager.getParty(uuid);
-            Text hhmessage = Text.of("hi");
-            logger.info((String.valueOf(playerParty.getAll().length)));
+            //TODO: Set up party formatter in PokeData like Stats formatter
         }
     }
 }
